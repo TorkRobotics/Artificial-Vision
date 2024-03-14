eb85f8bd89f0a2402340edfd3392e2d55cfec596
TorkRobotics <torkrobotics@gmail.com>
2024-03-13 21:45:01 -0600

    main.py file modification, better options organization

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from utils.train import run as RunTrain
 
-print('Select an option to run:')
-print('0: Train a model     1: Use trained model    2: Real time object detection')
+print('Choose an action.')
+print('0: Train a model                     1: Use trained model')
+print('2: Real time object detection        3: Display available cameras')
 selection = input('>_ ')
 
 if(selection == '0'):
